671262be33d6af6b0f8044714acb712cda6a86ec
tyj2001 <139536726+tyj2001@users.noreply.github.com>
2023-07-27 01:13:54 +0800

    Update i18n.js

--- i18n.js
+++ i18n.js
@@ -1,17 +1,22 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 
+// 导入你的翻译文件
+import enTranslation from './src/modules/llms/localai/en/translation.json';
+import zhTranslation from './src/modules/llms/localai/zh/translation.json';
+
+// 初始化i18n实例
 i18n.use(initReactI18next).init({
   resources: {
     en: {
-      translation: require('./src/modules/llms/localai/en/translation.json'),
+      translation: enTranslation,
     },
     zh: {
-      translation: require('./src/modules/llms/localai/zh/translation.json'),
+      translation: zhTranslation,
     },
   },
-  lng: "en",
-  fallbackLng: "en",
+  lng: 'en',
+  fallbackLng: 'en',
   interpolation: {
     escapeValue: false,
   },
