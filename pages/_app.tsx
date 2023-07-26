e106fb1ffb0d9cca5e9b7852b2d346e6b18ffd58
tyj2001 <139536726+tyj2001@users.noreply.github.com>
2023-07-27 01:35:03 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,7 @@ import '~/common/styles/GithubMarkdown.css';
 import { Brand } from '~/common/brand';
 import { createEmotionCache, theme } from '~/common/theme';
 
+import i18n from '../i18n'; // 确保从正确的路径导入 i18n 文件
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -31,23 +32,37 @@ function MyApp({ Component, emotionCache = clientSideEmotionCache, pageProps }: 
       },
     },
   }));
-  return <>
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <title>{Brand.Title.Common}</title>
-        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no' />
-      </Head>
-      {/* Rect-query provider */}
-      <QueryClientProvider client={queryClient}>
-        <CssVarsProvider defaultMode='light' theme={theme}>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
-          <Component {...pageProps} />
-        </CssVarsProvider>
-      </QueryClientProvider>
-    </CacheProvider>
-    <VercelAnalytics debug={false} />
-  </>;
+
+  // 使用状态来检测 i18n 是否初始化完成
+  const [i18nInitialized, setI18nInitialized] = React.useState(false);
+
+  // 在组件挂载时初始化 i18n
+  React.useEffect(() => {
+    i18n.init().then(() => {
+      setI18nInitialized(true);
+    });
+  }, []);
+
+  // 只有当 i18n 初始化完成后才渲染组件
+  return (
+    <>
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <title>{Brand.Title.Common}</title>
+          <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no' />
+        </Head>
+        {/* Rect-query provider */}
+        <QueryClientProvider client={queryClient}>
+          <CssVarsProvider defaultMode='light' theme={theme}>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            {i18nInitialized && <Component {...pageProps} />}
+          </CssVarsProvider>
+        </QueryClientProvider>
+      </CacheProvider>
+      <VercelAnalytics debug={false} />
+    </>
+  );
 }
 
 // enables the react-query api invocation
