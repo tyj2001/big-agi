350788a098cb897796006e7dfcf6f33e5bd1118b
tyj2001 <139536726+tyj2001@users.noreply.github.com>
2023-07-27 11:44:49 +0800

    Update ToolsSettings.tsx

--- src/apps/settings-modal/ToolsSettings.tsx
+++ src/apps/settings-modal/ToolsSettings.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import { FormHelperText, Stack } from '@mui/joy';
 
@@ -7,18 +8,19 @@ import { GoogleSearchSettings } from '~/modules/google/GoogleSearchSettings';
 import { settingsGap } from '~/common/theme';
 
 export function ToolsSettings() {
+  // 使用 useTranslation 钩子函数
+  const { t } = useTranslation();
 
   return (
-
-    <Stack direction='column' sx={{ gap: settingsGap }}>
-
+    <Stack direction="column" sx={{ gap: settingsGap }}>
+      {/* 使用 t 函数进行翻译 */}
       <FormHelperText>
-        🛠️ Tools enable additional capabilities if enabled and correctly configured
+        {t(
+          '🛠️ Tools enable additional capabilities if enabled and correctly configured'
+        )}
       </FormHelperText>
 
       <GoogleSearchSettings />
-
     </Stack>
-
   );
 }
