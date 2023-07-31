8c1b1935a4f3a2a94c791376e725c432bba266ef
tyj2001 <139536726+tyj2001@users.noreply.github.com>
2023-08-01 02:45:19 +0800

    Update MyComponent.js

--- MyComponent.js
+++ MyComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
-import { SystemPurposes, SystemPurposeId } from './src/data.ts';
+import { useTranslation, I18nextProvider } from 'react-i18next'; // 导入 I18nextProvider
+import { SystemPurposes, SystemPurposeId } from './src/data'; // 修正路径
+import i18n from '../i18n'; // 修正路径
 
 const MyComponent: React.FC = () => {
   const { t } = useTranslation();
@@ -21,15 +22,17 @@ const MyComponent: React.FC = () => {
   };
 
   return (
-    <div>
-      {renderSystemPurpose('Developer')}
-      {renderSystemPurpose('Scientist')}
-      {renderSystemPurpose('Catalyst')}
-      {renderSystemPurpose('Executive')}
-      {renderSystemPurpose('Designer')}
-      {renderSystemPurpose('Generic')}
-      {renderSystemPurpose('Custom')}
-    </div>
+    <I18nextProvider i18n={i18n}> {/* 将 MyComponent 包裹在 I18nextProvider 中 */}
+      <div>
+        {renderSystemPurpose('Developer')}
+        {renderSystemPurpose('Scientist')}
+        {renderSystemPurpose('Catalyst')}
+        {renderSystemPurpose('Executive')}
+        {renderSystemPurpose('Designer')}
+        {renderSystemPurpose('Generic')}
+        {renderSystemPurpose('Custom')}
+      </div>
+    </I18nextProvider>
   );
 };
 
