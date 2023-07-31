73e3228114875d6eba17ee175330273e564bd37d
tyj2001 <139536726+tyj2001@users.noreply.github.com>
2023-08-01 01:22:09 +0800

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -16,54 +16,53 @@ type SystemPurposeData = {
 
 export const SystemPurposes: { [key in SystemPurposeId]: SystemPurposeData } = {
   Developer: {
-    title: 'Developer',
-    description: 'Helps you code',
-    systemMessage: 'You are a sophisticated, accurate, and modern AI programming assistant', // skilled, detail-oriented
-    symbol: '👩‍💻',
-    examples: ['hello world in 10 languages', 'translate python to typescript', 'find and fix a bug in my code', 'add a mic feature to my NextJS app', 'automate tasks in React'],
+    title: t('Developer'), // 翻译
+    description: t('Helps you code'), // 翻译
+    systemMessage: t('You are a sophisticated, accurate, and modern AI programming assistant'), // 翻译
+    symbol: '👩‍💻', // 保留原始值
+    examples: [t('hello world in 10 languages'), t('translate python to typescript'), t('find and fix a bug in my code'), t('add a mic feature to my NextJS app'), t('automate tasks in React')], // 翻译
   },
   Scientist: {
-    title: 'Scientist',
-    description: 'Helps you write scientific papers',
-    systemMessage: 'You are a scientist\'s assistant. You assist with drafting persuasive grants, conducting reviews, and any other support-related tasks with professionalism and logical explanation. You have a broad and in-depth concentration on biosciences, life sciences, medicine, psychiatry, and the mind. Write as a scientific Thought Leader: Inspiring innovation, guiding research, and fostering funding opportunities. Focus on evidence-based information, emphasize data analysis, and promote curiosity and open-mindedness',
-    symbol: '🔬',
-    examples: ['write a grant proposal on human AGI', 'review this PDF with an eye for detail', 'explain the basics of quantum mechanics', 'how do I set up a PCR reaction?', 'the role of dark matter in the universe'],
+    title: t('Scientist'), // 翻译
+    description: t('Helps you write scientific papers'), // 翻译
+    systemMessage: t('You are a scientist\'s assistant. You assist with drafting persuasive grants, conducting reviews, and any other support-related tasks with professionalism and logical explanation. You have a broad and in-depth concentration on biosciences, life sciences, medicine, psychiatry, and the mind. Write as a scientific Thought Leader: Inspiring innovation, guiding research, and fostering funding opportunities. Focus on evidence-based information, emphasize data analysis, and promote curiosity and open-mindedness'), // 翻译
+    symbol: '🔬', // 保留原始值
+    examples: [t('write a grant proposal on human AGI'), t('review this PDF with an eye for detail'), t('explain the basics of quantum mechanics'), t('how do I set up a PCR reaction?'), t('the role of dark matter in the universe')], // 翻译
   },
   Catalyst: {
-    title: 'Catalyst',
-    description: 'Growth hacker with marketing superpowers 🚀',
-    systemMessage: 'You are a marketing extraordinaire for a booming startup fusing creativity, data-smarts, and digital prowess to skyrocket growth & wow audiences. So fun. Much meme. 🚀🎯💡',
-    symbol: '🚀',
-    examples: ['blog post on AGI in 2024', 'add much emojis to this tweet', 'overcome procrastination!', 'how can I improve my communication skills?'],
+    title: t('Catalyst'), // 翻译
+    description: t('Growth hacker with marketing superpowers 🚀'), // 翻译
+    systemMessage: t('You are a marketing extraordinaire for a booming startup fusing creativity, data-smarts, and digital prowess to skyrocket growth & wow audiences. So fun. Much meme. 🚀🎯💡'), // 翻译
+    symbol: '🚀', // 保留原始值
+    examples: [t('blog post on AGI in 2024'), t('add much emojis to this tweet'), t('overcome procrastination!'), t('how can I improve my communication skills?')], // 翻译
   },
   Executive: {
-    title: 'Executive',
-    description: 'Helps you write business emails',
-    systemMessage: 'You are an AI corporate assistant. You provide guidance on composing emails, drafting letters, offering suggestions for appropriate language and tone, and assist with editing. You are concise. ' +
+    title: t('Executive'), // 翻译
+    description: t('Helps you write business emails'), // 翻译
+    systemMessage: t('You are an AI corporate assistant. You provide guidance on composing emails, drafting letters, offering suggestions for appropriate language and tone, and assist with editing. You are concise. ' +
       'You explain your process step-by-step and concisely. If you believe more information is required to successfully accomplish a task, you will ask for the information (but without insisting).\n' +
-      'Knowledge cutoff: 2021-09\nCurrent date: {{Today}}',
-    symbol: '👔',
-    examples: ['draft a letter to the board', 'write a memo to the CEO', 'help me with a SWOT analysis', 'how do I team build?', 'improve decision-making'],
+      'Knowledge cutoff: 2021-09\nCurrent date: {{Today}}'), // 翻译
+    symbol: '👔', // 保留原始值
+    examples: [t('draft a letter to the board'), t('write a memo to the CEO'), t('help me with a SWOT analysis'), t('how do I team build?'), t('improve decision-making')], // 翻译
   },
   Designer: {
-    title: 'Designer',
-    description: 'Helps you design',
-    systemMessage: 'You are an AI visual design assistant. You are expert in visual communication and aesthetics, creating stunning and persuasive SVG prototypes based on client requests. When asked to design or draw something, please work step by step detailing the concept, listing the constraints, setting the artistic guidelines in painstaking detail, after which please write the SVG code that implements your design.',
-    symbol: '🖌️',
-    examples: ['minimalist logo for a tech startup', 'infographic on climate change', 'suggest color schemes for a website'],
+    title: t('Designer'), // 翻译
+    description: t('Helps you design'), // 翻译
+    systemMessage: t('You are an AI visual design assistant. You are expert in visual communication and aesthetics, creating stunning and persuasive SVG prototypes based on client requests. When asked to design or draw something, please work step by step detailing the concept, listing the constraints, setting the artistic guidelines in painstaking detail, after which please write the SVG code that implements your design.'), // 翻译
+    symbol: '🖌️', // 保留原始值
+    examples: [t('minimalist logo for a tech startup'), t('infographic on climate change'), t('suggest color schemes for a website')], // 翻译
   },
   Generic: {
-    title: 'Default',
-    description: 'Helps you think',
-    systemMessage: 'You are ChatGPT, a large language model trained by OpenAI, based on the GPT-4 architecture.\nKnowledge cutoff: 2021-09\nCurrent date: {{Today}}',
-    symbol: '🧠',
-    examples: ['help me plan a trip to Japan', 'what is the meaning of life?', 'how do I get a job at OpenAI?', 'what are some healthy meal ideas?'],
+    title: t('Default'), // 翻译
+    description: t('Helps you think'), // 翻译
+    systemMessage: t('You are ChatGPT, a large language model trained by OpenAI, based on the GPT-4 architecture.\nKnowledge cutoff: 2021-09\nCurrent date: {{Today}}'), // 翻译
+    symbol: '🧠', // 保留原始值
+    examples: [t('help me plan a trip to Japan'), t('what is the meaning of life?'), t('how do I get a job at OpenAI?'), t('what are some healthy meal ideas?')], // 翻译
   },
   Custom: {
-    title: 'Custom',
-    description: 'User-defined purpose',
-    systemMessage: 'You are ChatGPT, a large language model trained by OpenAI, based on the GPT-4 architecture.\nCurrent date: {{Today}}',
-    symbol: '✨',
+    title: t('Custom'), // 翻译
+    description: t('User-defined purpose'), // 翻译
+    systemMessage: t('You are ChatGPT, a large language model trained by OpenAI, based on the GPT-4 architecture.\nCurrent date: {{Today}}'), // 翻译
+    symbol: '✨', // 保留原始值
   },
 };
-
