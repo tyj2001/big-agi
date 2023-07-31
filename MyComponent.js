7b403ae704e5c2569ca1cee5d7f381ac9077cf16
tyj2001 <139536726+tyj2001@users.noreply.github.com>
2023-08-01 02:56:01 +0800

    Update MyComponent.js

--- MyComponent.js
+++ MyComponent.js
@@ -1,32 +1,30 @@
-import React from 'react';
-import { useTranslation, I18nextProvider } from 'react-i18next'; // 导入 I18nextProvider
-import { SystemPurposes, SystemPurposeId } from './src/data'; // 修正路径
-import i18n from '../i18n'; // 修正路径
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 
-const MyComponent: React.FC = () => {
-  const { t } = useTranslation();
+import enTranslation from './src/modules/llms/localai/en/translation.json'; // 修正路径
+import zhTranslation from './src/modules/llms/localai/zh/translation.json'; // 修正路径
 
-  const renderSystemPurpose = (purposeId: SystemPurposeId) => {
-    const purpose = SystemPurposes[purposeId];
-    return (
-      <div>
-        <h2>{t(purpose.title)}</h2>
-        <p>{t(purpose.description)}</p>
-        <p>{t(purpose.systemMessage)}</p>
-        <p>{purpose.symbol}</p>
-        {purpose.examples && purpose.examples.map((example, index) => (
-          <p key={index}>{t(example)}</p>
-        ))}
-      </div>
-    );
-  };
+const i18nInstance = i18n.createInstance();
 
-  return (
-    <I18nextProvider i18n={i18n}> {/* 将 MyComponent 包裹在 I18nextProvider 中 */}
-      <div>
-        {renderSystemPurpose('Developer')}
-        {renderSystemPurpose('Scientist')}
-        {renderSystemPurpose('Catalyst')}
+i18nInstance
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: enTranslation,
+      },
+      zh: {
+        translation: zhTranslation,
+      },
+    },
+    lng: "zh",
+    fallbackLng: "zh",
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18nInstance;
         {renderSystemPurpose('Executive')}
         {renderSystemPurpose('Designer')}
         {renderSystemPurpose('Generic')}
