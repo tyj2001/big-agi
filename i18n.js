1243596d0e834365a4094b6f52ac32ddec232f8c
tyj2001 <139536726+tyj2001@users.noreply.github.com>
2023-08-02 02:53:33 +0800

    Update i18n.js

--- i18n.js
+++ i18n.js
@@ -2,10 +2,10 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 // 根据你的项目结构，修正以下路径
-import enSystemPurposes from './modules/llms/localai/en/systemPurposes.json';
-import zhSystemPurposes from './modules/llms/localai/zh/systemPurposes.json';
-import enTranslation from './modules/llms/localai/en/translation.json';
-import zhTranslation from './modules/llms/localai/zh/translation.json';
+import enSystemPurposes from './src/modules/llms/localai/en/systemPurposes.json';
+import zhSystemPurposes from './src/modules/llms/localai/zh/systemPurposes.json';
+import enTranslation from './src/modules/llms/localai/en/translation.json';
+import zhTranslation from './src/modules/llms/localai/zh/translation.json';
 
 const i18nInstance = i18n.createInstance();
 
