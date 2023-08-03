c1d33abd8834a0bdfa015f79152bfb21ff9cd8d3
tyj2001 <139536726+tyj2001@users.noreply.github.com>
2023-08-04 03:23:38 +0800

    Update ExampleComponent.js

--- src/ExampleComponent.js
+++ src/ExampleComponent.js
@@ -9,23 +9,19 @@ const ExampleComponent = () => {
     i18n.changeLanguage(lng);
   };
 
-  const renderSystemPurpose = (systemPurposeId) => {
-    const systemPurpose = useSystemPurposeTranslation(systemPurposeId);
-
-    if (!systemPurpose) {
-      return null;
-    }
+  const RenderSystemPurpose = ({ key }) => {
+    const systemPurpose = useSystemPurposeTranslation(key);
 
     return (
-      <div key={systemPurposeId}>
-        <h1>{systemPurpose.title}</h1>
-        <p>{systemPurpose.description}</p>
-        <p>{systemPurpose.systemMessage}</p>
-        <p>{systemPurpose.symbol}</p>
+      <div key={key}>
+        <h1>{t(`SystemPurposes.${key}.title`)}</h1>
+        <p>{t(`SystemPurposes.${key}.description`)}</p>
+        <p>{t(`SystemPurposes.${key}.systemMessage`)}</p>
+        <p>{t(`SystemPurposes.${key}.symbol`)}</p>
         <ul>
           {systemPurpose.examples &&
             systemPurpose.examples.map((example, index) => (
-              <li key={index}>{example}</li>
+              <li key={index}>{t(example)}</li>
             ))}
         </ul>
       </div>
@@ -37,9 +33,9 @@ const ExampleComponent = () => {
       <button onClick={() => changeLanguage('en')}>English</button>
       <button onClick={() => changeLanguage('zh')}>中文</button>
 
-      {Object.keys(SystemPurposeId).map((systemPurposeId) =>
-        renderSystemPurpose(systemPurposeId)
-      )}
+      {Object.keys(SystemPurposes).map((key) => (
+        <RenderSystemPurpose key={key} key={key} />
+      ))}
     </div>
   );
 };
