9b8c4b25c6db13fe9e8cbcf43afc1ac49f9c3267
tyj2001 <139536726+tyj2001@users.noreply.github.com>
2023-08-05 05:23:47 +0800

    Update i18n.js

--- i18n.js
+++ i18n.js
@@ -2,8 +2,6 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 // 根据你的项目结构，修正以下路径
-import enSystemPurposes from './src/modules/llms/localai/en/systemPurposes.json';
-import zhSystemPurposes from './src/modules/llms/localai/zh/systemPurposes.json';
 import enTranslation from './src/modules/llms/localai/en/translation.json';
 import zhTranslation from './src/modules/llms/localai/zh/translation.json';
 
