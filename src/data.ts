48beaca1aac8e08dc5cf1045901ac43365078143
tyj2001 <139536726+tyj2001@users.noreply.github.com>
2023-08-04 14:08:32 +0800

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -16,80 +16,51 @@ type SystemPurposeData = {
 
 export const SystemPurposes: { [key in SystemPurposeId]: SystemPurposeData } = {
   Developer: {
-    title: 'Developer',
-    description: 'Helps you code',
-    systemMessage: 'You are a sophisticated, accurate, and modern AI programming assistant',
+    title: useTranslation('Developer.title'),
+    description: useTranslation('Developer.description'),
+    systemMessage: useTranslation('Developer.systemMessage'),
     symbol: '👩‍💻',
-    examples: ['hello world in 10 languages', 'translate python to typescript', 'find and fix a bug in my code', 'add a mic feature to my NextJS app', 'automate tasks in React']
+    examples: useTranslation('Developer.examples', { returnObjects: true }),
   },
   Scientist: {
-    title: 'Scientist',
-    description: 'Helps you write scientific papers',
-    systemMessage: 'You are a scientist\'s assistant. You assist with drafting persuasive grants, conducting reviews, and any other support-related tasks with professionalism and logical explanation. You have a broad and in-depth concentration on biosciences, life sciences, medicine, psychiatry, and the mind. Write as a scientific Thought Leader: Inspiring innovation, guiding research, and fostering funding opportunities. Focus on evidence-based information, emphasize data analysis, and promote curiosity and open-mindedness',
+    title: useTranslation('Scientist.title'),
+    description: useTranslation('Scientist.description'),
+    systemMessage: useTranslation('Scientist.systemMessage'),
     symbol: '🔬',
-    examples: ['write a grant proposal on human AGI', 'review this PDF with an eye for detail', 'explain the basics of quantum mechanics', 'how do I set up a PCR reaction?', 'the role of dark matter in the universe']
+    examples: useTranslation('Scientist.examples', { returnObjects: true }),
   },
   Catalyst: {
-    title: 'Catalyst',
-    description: 'Growth hacker with marketing superpowers 🚀',
-    systemMessage: 'You are a marketing extraordinaire for a booming startup fusing creativity, data-smarts, and digital prowess to skyrocket growth & wow audiences. So fun. Much meme. 🚀🎯💡',
+    title: useTranslation('Catalyst.title'),
+    description: useTranslation('Catalyst.description'),
+    systemMessage: useTranslation('Catalyst.systemMessage'),
     symbol: '🚀',
-    examples: ['blog post on AGI in 2024', 'add much emojis to this tweet', 'overcome procrastination!', 'how can I improve my communication skills?']
+    examples: useTranslation('Catalyst.examples', { returnObjects: true }),
   },
   Executive: {
-    title: 'Executive',
-    description: 'Helps you write business emails',
-    systemMessage: 'You are an AI corporate assistant. You provide guidance on composing emails, drafting letters, offering suggestions for appropriate language and tone, and assist with editing. You are concise. ' +
-      'You explain your process step-by-step and concisely. If you believe more information is required to successfully accomplish a task, you will ask for the information (but without insisting).\n' +
-      'Knowledge cutoff: 2021-09\nCurrent date: {{Today}}',
+    title: useTranslation('Executive.title'),
+    description: useTranslation('Executive.description'),
+    systemMessage: useTranslation('Executive.systemMessage'),
     symbol: '👔',
-    examples: ['draft a letter to the board', 'write a memo to the CEO', 'help me with a SWOT analysis', 'how do I team build?', 'improve decision-making']
+    examples: useTranslation('Executive.examples', { returnObjects: true }),
   },
   Designer: {
-    title: 'Designer',
-    description: 'Helps you design',
-    systemMessage: 'You are an AI visual design assistant. You are expert in visual communication and aesthetics, creating stunning and persuasive SVG prototypes based on client requests. When asked to design or draw something, please work step by step detailing the concept, listing the constraints, setting the artistic guidelines in painstaking detail, after which please write the SVG code that implements your design.',
+    title: useTranslation('Designer.title'),
+    description: useTranslation('Designer.description'),
+    systemMessage: useTranslation('Designer.systemMessage'),
     symbol: '🖌️',
-    examples: ['minimalist logo for a tech startup', 'infographic on climate change', 'suggest color schemes for a website']
+    examples: useTranslation('Designer.examples', { returnObjects: true }),
   },
   Generic: {
-    title: 'Default',
-    description: 'Helps you think',
-    systemMessage: 'You are ChatGPT, a large language model trained by OpenAI, based on the GPT-4 architecture.\nKnowledge cutoff: 2021-09\nCurrent date: {{Today}}',
+    title: useTranslation('Generic.title'),
+    description: useTranslation('Generic.description'),
+    systemMessage: useTranslation('Generic.systemMessage'),
     symbol: '🧠',
-    examples: ['help me plan a trip to Japan', 'what is the meaning of life?', 'how do I get a job at OpenAI?', 'what are some healthy meal ideas?']
+    examples: useTranslation('Generic.examples', { returnObjects: true }),
   },
   Custom: {
-    title: 'Custom',
-    description: 'User-defined purpose',
-    systemMessage: 'You are ChatGPT, a large language model trained by OpenAI, based on the GPT-4 architecture.\nCurrent date: {{Today}}',
-    symbol: '✨'
+    title: useTranslation('Custom.title'),
+    description: useTranslation('Custom.description'),
+    systemMessage: useTranslation('Custom.systemMessage'),
+    symbol: '✨',
   },
 };
-
-export function TranslatedSystemPurposes() {
-  const { t } = useTranslation();
-
-  return (
-    <div>
-      {Object.keys(SystemPurposes).map((key) => {
-        const purpose = SystemPurposes[key as SystemPurposeId];
-        return (
-          <div key={key}>
-            <h2>{t(purpose.title)}</h2>
-            <p>{t(purpose.description)}</p>
-            <p>{t(purpose.systemMessage)}</p>
-            <p>{purpose.symbol}</p>
-            {purpose.examples && (
-              <ul>
-                {purpose.examples.map((example) => (
-                  <li key={example}>{t(example)}</li>
-                ))}
-              </ul>
-            )}
-          </div>
-        );
-      })}
-    </div>
-  );
-}
