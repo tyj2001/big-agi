ba01a0cc94e996d6dd7d4514f8b6bccab946cd6e
tyj2001 <139536726+tyj2001@users.noreply.github.com>
2023-08-05 15:01:29 +0800

    Update i18n.js

--- i18n.js
+++ i18n.js
@@ -1,34 +1,26 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-// 根据你的项目结构，修正以下路径
 import enTranslation from './src/modules/llms/localai/en/translation.json';
 import zhTranslation from './src/modules/llms/localai/zh/translation.json';
 
-const i18nInstance = i18n.createInstance();
-
-i18nInstance
-  .use(initReactI18next)
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources: {
       en: {
-        translation: {
-          ...enTranslation,
-         
-        },
+        translation: enTranslation,
       },
       zh: {
-        translation: {
-          ...zhTranslation,
-          
-        },
+        translation: zhTranslation,
       },
     },
     lng: "zh",
     fallbackLng: "zh",
+
     interpolation: {
-      escapeValue: false,
-    },
+      escapeValue: false
+    }
   });
 
-export default i18nInstance;
+export default i18n;
